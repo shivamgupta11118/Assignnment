0a25aaf32bf162d77f2a7b3882838f88c22c1f16
Shivam Gupta <Shivamgupta11118@gmail.com>
2021-07-06 14:17:15 +0530

    6th-july-2020

---
+++ Project.java
@@ -0,0 +1,11 @@
+
+public class Project {
+	int projectId;
+	String name;
+	double budget;
+	public Project(int projectId,String name,double budget) {
+		this.projectId=projectId;
+		this.name=name;
+		this.budget=budget;
+	}
+}
