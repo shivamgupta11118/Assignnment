9394bc5d284845de5ca25606a491adf5f2933ac0
Shivam Gupta <Shivamgupta11118@gmail.com>
2021-07-15 12:55:39 +0530

    FutureTask

--- FibinacciWithCallable.java
+++ FibinacciWithCallable.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 //import sun.tools.serialver.resources.serialver;
 
@@ -14,19 +15,39 @@ public class FibinacciWithCallable {
 		ExecutorService service=Executors.newSingleThreadExecutor();
 		CallService c=new CallService();
 		Future<List<Integer>> call=service.submit(c);
+		//System.out.println("   ");
+		//Thread.sleep(100);
+		
 		try {
+			Thread.sleep(100);
 			if(call.isDone()) {
 				List<Integer>ll=call.get();
 //				System.out.println("Futue"+"----"+ll.get(0));
 				ll.forEach(System.out::println);
 			}
-			
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		System.out.println("----------");
+		FutureTask<List<Integer>>futureTask=new FutureTask<>(c);
+		Thread t=new Thread(futureTask,"CallableT1");
+		t.start();
+		try {
+			List<Integer> ll1=new ArrayList<>();
+			Thread.sleep(100);
+			if(futureTask.isDone()) {
+				ll1=futureTask.get();
+				ll1.forEach(System.out::println);
+			}
 		}catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
+		
 		service.shutdown();
-	}
+}
 
 }
 class CallService implements Callable<List<Integer>>{
